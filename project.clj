d7814ea4977c4c3ce9ebbf441842af25deebd86c
Guillaume Carbonneau <guillaume@curbside.com>
2020-08-10 12:14:10 -0400

    Version 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject curbside/timbre-json-output-fn "1.0.0-SNAPSHOT"
+(defproject curbside/timbre-json-output-fn "1.0.0"
   :description "Standardized structured logging for datadog"
   :url "http://github.com/RakutenReady/timbre-json-output-fn"
 
